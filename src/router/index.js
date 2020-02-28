5809d85f8027fbabb4474a94ab3b9190a9fe6bed
GH <2515911847@qq.com>
2020-02-28 08:53:56 +0800

    '完成路由进度条功能'

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 // 导入vue
 import Vue from 'vue'
 
+// 导NProgress的包
+import NProgress from 'nprogress';
+// 导NProgress的样式
+import 'nprogress/nprogress.css'
+
 //导入路由
 import VueRouter from 'vue-router'
 
@@ -54,5 +59,21 @@ const router = new VueRouter({
   routes
 })
 
-// 暴露出去
+// 导航守卫写在router对象的后面
+// 即将跳转路由之前调用
+// to: 到哪去
+// from: 从哪来
+// next：函数,调用函数就跳转(代表放行)
+router.beforeEach((to,from,next) => {
+  // 开启进度条
+  NProgress.start();
+  next();
+})
+ 
+// 跳转之后调用 
+router.afterEach(() => {
+  // 结束进度条
+  NProgress.done();
+})
+// 暴露出去 
 export default router
